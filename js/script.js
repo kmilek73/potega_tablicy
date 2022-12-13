e43d376eb2b4ae8eb4a77700d4fc708124abd1fb
kmilek <kmilek73@gmail.com>
2022-12-13 11:13:11 +0100

    rozwiazanie z poprawkami

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-let numbers = [0,1,2,3,4,5];
-let dlugoscTablicy = numbers.length;
-let kwadrat = 0;
-let sumaWszystkich=0;
+let numbers = [0, 1, 2, 3, 4, 5];
+;
 
 
-function sumaAll(){
-
-for (let i=0; i<dlugoscTablicy; i++){
-   kwadrat = Math.pow(numbers[i],2);
-    console.log("Kwadrat liczby ", numbers[i],"wynosi : ",kwadrat);
-    sumaWszystkich = sumaWszystkich + kwadrat;
-    }
-     return sumaWszystkich;
+function sumaAll(numbers) {
+   let dlugoscTablicy = numbers.length;
+   let kwadrat = 0;
+   let sumaWszystkich = 0
+   for (let i = 0; i < dlugoscTablicy; i++) {
+      kwadrat = Math.pow(numbers[i], 2);
+      console.log("Kwadrat liczby ", numbers[i], "wynosi : ", kwadrat);
+      sumaWszystkich = sumaWszystkich + kwadrat;
+   }
+   return sumaWszystkich;
 }
 
-console.log("Suma wszystkich kwadratów wynosi : ",sumaAll(numbers));
+console.log("Suma wszystkich kwadratów wynosi : ", sumaAll(numbers));
